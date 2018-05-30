ab58a4f6c2ea396f20ce651b58dd7104527969da
vagrant <vagrant@localhost.localdomain>
2018-05-30 01:37:20 +0000

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
+set -o pipefail
+
+branch=$1
+
+git branch -ar
+git checkout $branch
+git pull
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	dest=`echo $line | awk -F'/' '{print $(NF-1)}'`
 	sudo mkdir /vagrant/alc/kiran/stage/$dest 2>/dev/null
 	if [[ $dest == $dest ]]; then
-		cp -r $line /vagrant/alc/kiran/stage/${dest}
+		cp $line /vagrant/alc/kiran/stage/${dest}
 	fi
 done < "alc.txt"
